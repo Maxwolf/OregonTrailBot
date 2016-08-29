cd7f01077181abed4ed3d44844ea7843dc18a4fe
Maxwolf <ron.mcdowell@gmail.com>
2016-08-29 16:31:00 -0700

    Merged efforts from oregon trail console game remake and fate bot project I made into a single project with one console application as the telegram bot controller server and the oregon trail simulation and wolf curses as class libraries.

---
+++ src/TrailBot/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TrailBot
+{
+    internal class Program
+    {
+        private static readonly TelegramBotClient Bot =
+            new TelegramBotClient("227031115:AAElTAMDxnUxJ5_VuDn5LUUvQpNHnyT_V2Q");
+
+        private static void Main(string[] args)
+        {
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.Title = me.Username;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Debugger.Break();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.TextMessage) return;
+
+            if (message.Text.StartsWith("/keyboard")) // send custom keyboard
+            {
+                var keyboard = new ReplyKeyboardMarkup(new[]
+                {
+                    new[] // first row
+                    {
+                        new KeyboardButton("1.1"),
+                        new KeyboardButton("1.2")
+                    },
+                    new[] // last row
+                    {
+                        new KeyboardButton("2.1"),
+                        new KeyboardButton("2.2")
+                    }
+                });
+
+                await Bot.SendTextMessageAsync(message.Chat.Id, "Choose",
+                    replyMarkup: keyboard);
+            }
+            else if (message.Text.StartsWith("/photo")) // send a photo
+            {
+                await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.UploadPhoto);
+
+                const string file = @"C:\OregonTrailBot\bin\test.jpg";
+
+                var fileName = file.Split('\\').Last();
+
+                using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    var fts = new FileToSend(fileName, fileStream);
+
+                    await Bot.SendPhotoAsync(message.Chat.Id, fts, "Nice Picture");
+                }
+            }
+            else
+            {
+                var usage = @"Usage:
+/keyboard - send custom keyboard
+/photo    - send a photo
+";
+
+                await Bot.SendTextMessageAsync(message.Chat.Id, usage,
+                    replyMarkup: new ReplyKeyboardHide());
+            }
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender,
+            CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            await Bot.AnswerCallbackQueryAsync(callbackQueryEventArgs.CallbackQuery.Id,
+                $"Received {callbackQueryEventArgs.CallbackQuery.Data}");
+        }
+    }
+}
